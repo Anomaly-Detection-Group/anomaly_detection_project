c443050ecd76dee7025fa6657d477639e7be6f4c
MigashaneVictoire <Migashanevictoire@gmail.com>
2023-08-09 20:39:21 -0500

    comple question one and two

--- Victoire/explore_victoire_.py
+++ Victoire/explore_victoire_.py
@@ -11,42 +11,41 @@ warnings.filterwarnings("ignore")
 
 sns.set_theme(style="whitegrid")
 
+# load cleaned data
+codeup = wrangle_victoire_.wrangle_codeup()
+
+# drop nulls in the path column
+codeup = codeup[~codeup.path.isna()]
+# remove bad urls from path
+codeup = codeup[~codeup.path.isin(["/"])]
+
+# create a lesson column
+codeup["lesson"] = codeup.path.str.extract('^(.*?)(?=\/)')
+
 # explore function
 def get_explore_question_1():
-    # load cleaned data
-    codeup = wrangle_victoire_.wrangle_codeup()
     
-    # drop nulls in the path column
-    codeup = codeup[~codeup.path.isna()]
-    # remove bad urls from path
-    codeup = codeup[~codeup.path.isin(["/"])]
-    
-    # create a lesson column
-    codeup["lesson"] = codeup.path.str.extract('^(.*?)(?=\/)')
-    
-    # group the dataframe
-    lesson_counts = codeup.groupby(["program_id","cohort_id","lesson"]).date.agg(["count"]).sort_values(by=["program_id"], ascending=False)
+    # Group by program_id, cohort_id, and lesson, then count the occurrences of each lesson
+    lesson_counts = codeup.groupby(["program_id","cohort_id", "lesson"])["lesson"].count().reset_index(name="referred_to_lesson_count")
 
-    # Reset the index and create columns for group keys
-    lesson_counts = lesson_counts.reset_index()
-    
-    # find name of highly visited lesson
-    lesson_counts = lesson_counts.groupby(["program_id","cohort_id"])["lesson","count"].max()
+    # Find the top ten lesson counts in each cohort_id within each program_id
+    lesson_counts = lesson_counts.sort_values(by=["program_id","cohort_id", "referred_to_lesson_count"], ascending=[True,True,False]).groupby(["cohort_id"]).head(1)
     return lesson_counts
 
 
 # explore function
 def get_explore_question_2():
-    # get engineered data
-    lesson_counts = get_explore_question_1()
+    # Group by program_id, cohort_id, and lesson, then count the occurrences of each lesson
+    lesson_counts = codeup.groupby(["cohort_id", "lesson"])["lesson"].count().reset_index(name="referred_to_lesson_count")
     
-    # which cohort visited web-design
-    web_design = lesson_counts[lesson_counts.lesson == "web-design"]
-    
-    # reset dataframe index
-    web_design = web_design.reset_index()
-    
-    # get min and max cohort counts
-    min_max_counts = web_design[(web_design["count"] == web_design["count"].max()) | (web_design["count"] == web_design["count"].min())]
+    # Find the top ten lesson counts in each cohort_id within each program_id
+    top_5_in_cohort = lesson_counts.sort_values(by=["cohort_id", "referred_to_lesson_count"], ascending=[True,False]).groupby(["cohort_id"]).head(5)
 
-    return min_max_counts
+    # look at one lesson to compare top results
+    plt.figure(figsize=(10,3))
+    top_5_in_cohort[top_5_in_cohort.lesson == "javascript-i"].referred_to_lesson_count.sort_values(ascending=False).plot(kind="bar")
+    plt.title("cohort reference to javascript-1 count")
+    plt.xlabel("cohort id")
+    plt.ylabel("referred javascript-i count")
+
+    return plt.gcf()
